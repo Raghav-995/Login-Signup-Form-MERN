87cb90ad233885116fb2b95fb0f8e7af85ab878b
Pathak Raghavendra <139675343+Raghav-995@users.noreply.github.com>
2023-11-06 20:29:24 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -12,7 +12,7 @@ const Login = () => {
     const handleSubmit = (event) => {
         event.preventDefault();
         axios.defaults.withCredentials = true;
-        axios.post( 'https://login-signup-backed.vercel.app/login', {email, password})
+        axios.post( 'https://login-signup-backend.vercel.app/login', {email, password})
         .then(result => {
             console.log(result);
             if(result.data === "Success"){
