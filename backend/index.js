ba5b98779ca2e2ffec772b13cb464c73ab6ad9a5
Pathak Raghavendra <139675343+Raghav-995@users.noreply.github.com>
2023-11-06 21:22:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -23,44 +23,6 @@ app.get('/',(req,res) => {
 res.send("Server Successfully Connected to https://localhost:3001");
 })
 
-app.post('/register', (req, res)=>{
-    // To post / insert data into database
-
-    const {email, password,name,date,city,country,gender,phone} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            res.json("Already registered")
-        }
-        else{
-            FormDataModel.create(req.body)
-            .then(log_reg_form => res.json(log_reg_form))
-            .catch(err => res.json(err))
-        }
-    })
-    
-})
-
-app.post('/login', (req, res)=>{
-    // To find record from the database
-    const {email, password} = req.body;
-    FormDataModel.findOne({email: email})
-    .then(user => {
-        if(user){
-            // If user found then these 2 cases
-            if(user.password === password) {
-                res.json("Success");
-            }
-            else{
-                res.json("Wrong password");
-            }
-        }
-        // If user not found then 
-        else{
-            res.json("No records found! ");
-        }
-    })
-})
 
 app.listen(3001, () => {
     console.log("Server listening on http://127.0.0.1:3001");
